2be36dd088d379e26682adee6b2e2d8a68e5e8b8
Victor <Victor@DESKTOP-JI227NH>
2023-08-22 21:32:58 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSound-OO/Musica.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound_OO
+{
+    internal class Musica
+    {
+        public string nome;
+        public string artista;
+        public int duracao;
+
+        public bool disponivel { get; set; } //Os Getters e Setters são declarados dessa forma
+
+
+        /* public void SetDisponivel(bool value)
+         {
+             disponivel = value;
+         }
+
+         public bool GetDisponivel()
+         {
+             return disponivel;
+         }*/
+
+        public void ExibirFichaTecnica()
+        {
+            Console.WriteLine($"Nome: {nome}");
+            Console.WriteLine($"Artista: {artista}");
+            Console.WriteLine($"Duração: {duracao}");
+            if (disponivel)
+            {
+                Console.WriteLine("Disponível no plano");
+            }
+            else
+            {
+                Console.WriteLine("Adquira o plano Plus+");
+            }
+        }
+
+    }
+}
---
+++ ScreenSound-OO/Program.cs
@@ -0,0 +1,16 @@
+﻿using ScreenSound_OO;
+
+Musica musica1 = new Musica();
+musica1.nome = "Roxanne";
+musica1.artista = "The Police";
+musica1.duracao = 273;
+musica1.disponivel = true; //Esse atributo usa Setter
+
+Musica musica2 = new Musica();
+musica2.nome = "Vertigo";
+musica2.artista = "U2";
+musica2.duracao = 324;
+musica2.disponivel = true;
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
